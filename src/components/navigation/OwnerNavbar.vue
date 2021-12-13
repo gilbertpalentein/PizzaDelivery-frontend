11f8a7430080089380bc1949cb45a2cef50b191f
Gilbert138 <gilbertpalentein8@gmail.com>
2021-12-13 23:04:11 +0700

    Integrasi logout pada semua navbar

--- src/components/navigation/OwnerNavbar.vue
+++ src/components/navigation/OwnerNavbar.vue
@@ -12,12 +12,7 @@
         <span style="margin-left: 0.5rem">Home</span>
       </b-navbar-item>
       <b-navbar-item href="/owner/log">
-        <b-icon
-          pack="fas"
-          icon="book-open"
-          size="is-small"
-          type="is-primary"
-        >
+        <b-icon pack="fas" icon="book-open" size="is-small" type="is-primary">
         </b-icon>
         <span style="margin-left: 0.5rem">View Transaction Log</span>
       </b-navbar-item>
@@ -41,11 +36,51 @@
         </b-icon>
         <span style="margin-left: 0.5rem">View Product Sold</span>
       </b-navbar-item>
-      <b-navbar-item href="#">
+      <b-navbar-item href="">
         <b-icon pack="fas" icon="user" size="is-small" type="is-primary">
         </b-icon>
-        <span style="margin-left: 0.5rem">Logout</span>
+        <span style="margin-left: 0.5rem" v-on:click="logout"
+          ><button class="btn">Logout</button></span
+        >
       </b-navbar-item>
     </template>
   </b-navbar>
 </template>
+<script>
+import axios from "axios";
+import LoginService from "@/services/LoginService";
+
+export default {
+  mounted() {
+    this.fetchData();
+  },
+  data: () => {
+    const data = [];
+    return {
+      userType: "",
+      loginService: new LoginService(),
+
+      data,
+    };
+  },
+  methods: {
+    async fetchData() {
+      await axios.get("/logout");
+    },
+    async logout() {
+      console.log("test");
+      this.loginService.removeUserType();
+      location.replace("/");
+    },
+  },
+};
+</script>
+<style scoped>
+.btn {
+  background-color: white;
+  border: none;
+  font-size: 15px;
+  font-weight: normal;
+  margin-left: -0.7%;
+}
+</style>
